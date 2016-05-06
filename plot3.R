341479681b42f8004c264938d4d8707e9b47bd0b
CaseyBrust <caseybrust@gmail.com>
2016-05-06 16:07:04 -0400

    Added files via upload

---
+++ plot3.R
@@ -0,0 +1,26 @@
+#download data
+if(!file.exists("projectdata.zip")) {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","projectdata.zip")
+} 
+if(!file.exists("household_power_consumption.txt")) {
+  unzip("projectdata.zip","household_power_consumption.txt")
+}
+
+#read data into R
+data <- read.table("household_power_consumption.txt",na.strings="\\?",header=FALSE,nrows=2879,skip=66637,sep=";")
+varnames <- read.table("household_power_consumption.txt",nrows=1,sep=";",header=TRUE)
+
+#assign column names
+colnames(data) <- colnames(varnames)
+
+#convert date & time variables to date format
+data$DateTime <- strptime(paste(data$Date,data$Time),format="%d/%m/%Y%H:%M:%S")
+
+#Plot 3
+png("plot3.png",width=480,height=480,units="px")
+plot(data$DateTime,data$Sub_metering_1,type="n",ylab="Energy sub metering",xlab="")
+lines(data$DateTime,data$Sub_metering_1,type="l")
+lines(data$DateTime,data$Sub_metering_2,type="l",col="red")
+lines(data$DateTime,data$Sub_metering_3,type="l",col="blue")
+legend("topright",colnames(data)[7:9],col=c("black","red","blue"),lty=1)
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,35 @@
+#download data
+if(!file.exists("projectdata.zip")) {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","projectdata.zip")
+} 
+if(!file.exists("household_power_consumption.txt")) {
+  unzip("projectdata.zip","household_power_consumption.txt")
+}
+
+#read data into R
+data <- read.table("household_power_consumption.txt",na.strings="\\?",header=FALSE,nrows=2879,skip=66637,sep=";")
+varnames <- read.table("household_power_consumption.txt",nrows=1,sep=";",header=TRUE)
+
+#assign column names
+colnames(data) <- colnames(varnames)
+
+#convert date & time variables to date format
+data$DateTime <- strptime(paste(data$Date,data$Time),format="%d/%m/%Y%H:%M:%S")
+
+#Plot 4
+png("plot4.png",width=480,height=480,units="px")
+par(mfrow=c(2,2),cex=.65)
+with(data, {
+  plot(DateTime,Global_active_power,type="n",ylab="Global Active Power (kilowatts)",xlab="")
+  lines(DateTime,Global_active_power,type="l")
+  plot(DateTime,Voltage,type="n",ylab="Voltage",xlab="datetime")
+  lines(DateTime,Voltage,type="l")
+  plot(DateTime,Sub_metering_1,type="n",ylab="Energy sub metering",xlab="")
+  lines(DateTime,Sub_metering_1,type="l")
+  lines(DateTime,Sub_metering_2,type="l",col="red")
+  lines(DateTime,Sub_metering_3,type="l",col="blue")
+  legend("topright",colnames(data)[7:9],col=c("black","red","blue"),lty=1,bty="n")
+  plot(DateTime,Global_reactive_power,type="n",ylab="Global_reactive_power",xlab="datetime")
+  lines(DateTime,Global_reactive_power,type="l")
+} )
+dev.off()
